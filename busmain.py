14c25f79fc093c9ef19271e213ce951c2afb92b4
loyen <loyenmarie.alegado@gmail.com>
2017-05-31 19:46:49 +0800

    'yo'

--- busmain.py
+++ busmain.py
@@ -55,6 +55,6 @@ def add_cors(resp):
 
 
 if __name__ == '__main__':
-app.debug=True
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    app.debug=True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
